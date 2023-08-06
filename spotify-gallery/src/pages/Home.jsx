59212e60efa377373a8460b79f37a3c1f4227219
Dorothea Lim Hui Ying <Dorothea@Internationals-MacBook-Pro.local>
2023-08-06 18:40:27 +0800

    added initial css

--- spotify-gallery/src/pages/Home.jsx
+++ spotify-gallery/src/pages/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
     const [clickTopArtists, setClickTopArtists] = useState(false);
     const [clickTopTracks, setClickTopTracks] = useState(false);
 
-    const TOP_TRACKS_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&offset=0&limit=12"
-    const TOP_ARTISTS_ENDPOINT = "https://api.spotify.com/v1/me/top/artists?time_range=short_term&offset=0&limit=12"
+    const TOP_TRACKS_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&offset=0&limit=10"
+    const TOP_ARTISTS_ENDPOINT = "https://api.spotify.com/v1/me/top/artists?time_range=short_term&offset=0&limit=10"
 
     const getParamsfromHash = (hash) => {
         const hashContent = hash.substring(1);
@@ -39,12 +39,14 @@ const Home = () => {
     }
 
     const getTopArtists = () => {
-        // setClickTopArtists(true);
+        setClickTopArtists(true);
+        setClickTopTracks(false);
         getData(TOP_ARTISTS_ENDPOINT, setTopArtists);
     }
 
     const getTopTracks = () => {
-        // setClickTopTracks(true);
+        setClickTopArtists(false);
+        setClickTopTracks(true);
         getData(TOP_TRACKS_ENDPOINT, setTopTracks);
     }
 
@@ -70,32 +72,53 @@ const Home = () => {
                 <h4>Discover Your Top Tracks & Artists</h4>
                 <button className='button' onClick={getTopArtists}>Top Artists</button>
                 <button className='button' onClick={getTopTracks}>Top Tracks</button>
-                {topTracks.items &&
+                {topTracks.items && clickTopTracks &&
                 <div>
+                <div className='customized-container'>
+                <div className='text'>Hi User Name!</div>
+                <div className='text'>Current Date and Time</div>
+                </div>
                 <h4>Top Tracks</h4>
                 <div>
                 {topTracks.items.map((track, index) => {
-                    return (
-                        <div key={index}>
-                            <img src={track.album.images[1].url} />
-                            <h5>{track.name}</h5>
-                        </div>
-                    )
+                    if(index === 0){
+                        return (
+                            <div key={index}>
+                                <img src={track.album.images[0].url} width={"300px"}/>
+                                <div>{track.name}</div>
+                            </div>
+                        )
+                    }else if(index >= 1){
+                        return (
+                            <div key={index} className='grid-container'>
+                                <img className='grid-img' src={track.album.images[2].url} width={"80px"}/>
+                                <div className='grid-title'>{track.name}</div>
+                            </div>
+                        )
+                    }
                 })}
                 </div>
                 </div>
                 }
-                {topArtists.items &&
+                {topArtists.items && clickTopArtists &&
                 <div>
                 <h4>Top Artist</h4>
                 <div>
                 {topArtists.items.map((artist, index) => {
+                     if(index === 0){
+                        return (
+                            <div key={index}>
+                                <img src={artist.images[0].url} width={"300px"}/>
+                                <div>{artist.name}</div>
+                            </div>
+                        )
+                    }else if(index >= 1){
                     return (
                         <div key={index}>
-                            <img src={artist.images[1].url} />
-                            <h5>{artist.name}</h5>
+                            <img src={artist.images[2].url} width={"80px"}/>
+                            <div>{artist.name}</div>
                         </div>
-                    )
+                    )}
                 })}
                 </div>
                 </div>
