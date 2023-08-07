78125f5a9537a013d86a197d78997b9d5866d173
Dorothea Lim Hui Ying <Dorothea@Internationals-MacBook-Pro.local>
2023-08-07 15:19:46 +0800

    added all time results feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { all } from 'axios';
 import { useEffect , useState} from 'react';
 
 
@@ -9,9 +9,14 @@ const Home = () => {
     const [topTracks, setTopTracks] = useState({});
     const [clickTopArtists, setClickTopArtists] = useState(false);
     const [clickTopTracks, setClickTopTracks] = useState(false);
+    const [lastMonthTracks, setLastMonthTracks] = useState(true);
+    const [allTimeTracks, setAllTimeTracks] = useState(false);
 
-    const TOP_TRACKS_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&offset=0&limit=10"
     const TOP_ARTISTS_ENDPOINT = "https://api.spotify.com/v1/me/top/artists?time_range=short_term&offset=0&limit=10"
+    const TOP_TRACKS_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&offset=0&limit=10"
+
+    const ALL_TIME_ARTISTS_ENDPOINT = "https://api.spotify.com/v1/me/top/artists?time_range=long_term&offset=0&limit=10"
+    const ALL_TIME_TRACKS_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks?time_range=long_term&offset=0&limit=10"
 
     const getParamsfromHash = (hash) => {
         const hashContent = hash.substring(1);
@@ -38,16 +43,47 @@ const Home = () => {
         })
     }
 
+
+    const getLastMonthsTracks = () => {
+        setAllTimeTracks(false);
+        setLastMonthTracks(true);
+        console.log("last month")
+        if(lastMonthTracks === true){
+            radioToggle();
+        }
+    }
+
+    const getAllTimeTracks = () => {
+        setLastMonthTracks(false);
+        setAllTimeTracks(true);
+        console.log("all time")
+        if(allTimeTracks === true){
+            radioToggle();
+        }
+    }
+
     const getTopArtists = () => {
         setClickTopArtists(true);
         setClickTopTracks(false);
         getData(TOP_ARTISTS_ENDPOINT, setTopArtists);
     }
 
+    const radioToggle = () => {
+        if(!lastMonthTracks){
+            getData(ALL_TIME_TRACKS_ENDPOINT, setTopTracks);
+            console.log(topTracks)
+        }else{
+            getData(TOP_TRACKS_ENDPOINT, setTopTracks); 
+            console.log(topTracks)
+        }
+    }
+
     const getTopTracks = () => {
         setClickTopArtists(false);
         setClickTopTracks(true);
-        getData(TOP_TRACKS_ENDPOINT, setTopTracks);
+        setLastMonthTracks(true);
+        setAllTimeTracks(false);
+        getData(TOP_TRACKS_ENDPOINT, setTopTracks); 
     }
 
     useEffect(() => {
@@ -66,32 +102,49 @@ const Home = () => {
     },[])
     return ( 
         <div>
+            <h2>Spotify Gallery</h2>
+                <h4>Discover Your Top Tracks & Artists</h4>
+                <button className='button' onClick={getTopArtists}>TOP ARTISTS</button>
+                <button className='button' onClick={getTopTracks}>TOP TRACKS</button>
             {token &&
             <div>
-                <h2>Spotify Gallery</h2>
-                <h4>Discover Your Top Tracks & Artists</h4>
-                <button className='button' onClick={getTopArtists}>Top Artists</button>
-                <button className='button' onClick={getTopTracks}>Top Tracks</button>
-                {topTracks.items && clickTopTracks &&
-                <div>
                 <div className='customized-container'>
                 <div className='text'>Hi User Name!</div>
                 <div className='text'>Current Date and Time</div>
                 </div>
-                <h4>Top Tracks</h4>
+                {topTracks.items && clickTopTracks &&
                 <div>
+                <div className='choice-container'>
+                <h5 id='choice-text'>Top Tracks</h5>
+
+                <input className='radio' type='radio' id='last-month' name='result-type' value="Last Month" onClick={getLastMonthsTracks}/>
+                <label className='radio'>Last Month</label>
+
+                <input className='radio' type='radio' id='all-time' name='result-type' value="All Time" onClick={getAllTimeTracks}/>
+                <label className='radio'>All Time</label>
+
+                {/* <input type='checkbox' id='btnControl' />
+                <label className='toggle-button' for="btnControl" onClick={getLastMonthsTracks}>Last Month</label>
+
+                <input type='checkbox' id='btnControl' />
+                <label className='toggle-button' for="btnControl" onClick={getAllTimeTracks}>All Time</label> */}
+
+                {/* <button className='toggle-button' id='month' onClick={getLastMonthsTracks}>Last Month</button>
+                <button className='toggle-button' onClick={getAllTimeTracks}>All Time</button> */}
+                </div>
+                <div className='grid-container'>
                 {topTracks.items.map((track, index) => {
                     if(index === 0){
                         return (
                             <div key={index}>
                                 <img src={track.album.images[0].url} width={"300px"}/>
-                                <div>{track.name}</div>
+                                <div className='grid-title'>{track.name}</div>
                             </div>
                         )
                     }else if(index >= 1){
                         return (
-                            <div key={index} className='grid-container'>
-                                <img className='grid-img' src={track.album.images[2].url} width={"80px"}/>
+                            <div key={index} className='item-container'>
+                                <img className='grid-img' src={track.album.images[1].url} width={"95px"}/>
                                 <div className='grid-title'>{track.name}</div>
                             </div>
                         )
@@ -102,27 +155,34 @@ const Home = () => {
                 }
                 {topArtists.items && clickTopArtists &&
                 <div>
-                <h4>Top Artist</h4>
-                <div>
+                <div className='choice-container'>
+                <h5>Top Artist</h5>
+                </div>
+                <div className='grid-container'>
                 {topArtists.items.map((artist, index) => {
                      if(index === 0){
                         return (
                             <div key={index}>
                                 <img src={artist.images[0].url} width={"300px"}/>
-                                <div>{artist.name}</div>
+                                <div className='grid-title'>{artist.name}</div>
                             </div>
                         )
                     }else if(index >= 1){
                     return (
-                        <div key={index}>
-                            <img src={artist.images[2].url} width={"80px"}/>
-                            <div>{artist.name}</div>
+                        <div key={index} className='item-container'>
+                            <img className='grid-img' src={artist.images[1].url} width={"95px"}/>
+                            <div className='grid-title'>{artist.name}</div>
                         </div>
-                    )}
+                    )
+                }
                 })}
                 </div>
                 </div>
                 }
+            <div className='selection-text'>
+                    {!clickTopArtists && !clickTopTracks &&
+                    <h5>Select Top Artists / Tracks to view your stats for the month</h5>}
+                </div>
             </div>
             }
         </div>
